35102efe394b500392582dc5196acb9b35271847
Tommy Unger <tommyunger@gmail.com>
2020-08-23 07:03:03 -0700

    working on search

--- qindex_db.py
+++ qindex_db.py
@@ -5,6 +5,7 @@ import sys
 import os
 import re
 import glob
+import random
 import math
 import ciso8601
 import multiprocessing
@@ -33,12 +34,13 @@ def csv_to_parq(fname):
    line_length = 0
    with open(fname) as f:
       for line in f:
-         line_num += 1
-         line_length += len(line)
-         if line_num >= 1000:
-            break
+         if random.random() < 0.005:
+            line_num += 1
+            line_length += len(line)
+            if line_num >= 100000:
+               break
 
-   rows_per_file = 5000000
+   rows_per_file = 6000000
    file_size = os.path.getsize(fname)
    avg_line_len = line_length / line_num
    files_needed = math.ceil((file_size / avg_line_len) / rows_per_file)
@@ -46,7 +48,7 @@ def csv_to_parq(fname):
    pool = multiprocessing.Pool(16)
    res = []
    file_id = 1
-   for file_id in range(0, files_needed+1):
+   for file_id in range(0, files_needed):
       # process_lines(fname, file_id, rows_per_file)
       res.append(pool.apply_async(process_lines, (fname, file_id, rows_per_file)))
    pool.close()
@@ -55,18 +57,22 @@ def csv_to_parq(fname):
 def search_file(file, s):
    df = pandas.read_parquet(file)
    found = df[df['name'].str.contains(s, na=False, flags=re.IGNORECASE, regex=True)]
-   return found
+   return {"results": found, "searched_count": len(df)}
 
-def search_parq(s):
+def search_parq(file, s):
    pool = multiprocessing.Pool(16)
    res = []
    file_id = 1
-   for f_name in glob.glob("output-walk-log.txt-*"):
+   for f_name in glob.glob("%s-*.parq" % file):
       res.append(pool.apply_async(search_file, (f_name, s)))
    pool.close()
    pool.join()
    for r in res:
-      print(r.get())
+      d = r.get()
+      for index, row in d["results"].iterrows():
+         print("%(id)12s - %(f_type)17s - %(path)s" % row)
 
-csv_to_parq(sys.argv[1])
-# search_parq("tommy")
+if sys.argv[1] == "import_initial":
+   csv_to_parq(sys.argv[2])
+elif sys.argv[1] == "search":
+   search_parq(sys.argv[2], sys.argv[3])
