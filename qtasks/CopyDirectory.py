601730b5d4c7400f265aca225c3d908f69e1f952
Tommy Unger <tommyunger@gmail.com>
2020-12-08 20:52:04 -0800

    catch all exceptions in copy

--- qtasks/CopyDirectory.py
+++ qtasks/CopyDirectory.py
@@ -4,6 +4,7 @@ import io
 import sys
 import argparse
 
+
 class CopyDirectory:
     def __init__(self, args):
         parser = argparse.ArgumentParser(description='')
@@ -47,42 +48,52 @@ class CopyDirectory:
     def every_batch(file_list, work_obj):
         results = []
         for file_obj in file_list:
-            to_path = file_obj['path'].replace(work_obj.start_path, work_obj.run_class.to_dir)
-            parent_path = os.path.dirname(to_path)
-            file_name = os.path.basename(to_path)
-            work_obj.run_class.create_folder(work_obj.rc, parent_path)
-            if file_obj['type'] == "FS_FILE_TYPE_DIRECTORY":
-                work_obj.run_class.create_folder(work_obj.rc, to_path)
-                results.append("DIRECTORY  : %s -> %s" % (file_obj['path'], to_path))
-            else:
-                try:
-                    new_f = work_obj.rc.fs.get_file_attr(path = to_path)
-                    if new_f['size'] == file_obj['size'] and \
-                            new_f['modification_time'] == file_obj['modification_time']:
-                        results.append("FILE EXISTS: %s -> %s" % (file_obj['path'], to_path))
-                        continue
-                    else:
-                        work_obj.rc.fs.delete(id_ = new_f['id'])
-                except:
-                    # print("File copy exception: %s %s" % (sys.exc_info()[0], file_obj['path']))
-                    pass
-                try:
-                    work_obj.rc.fs.create_file(dir_path=parent_path, name=file_name)
-                    work_obj.rc.fs.copy(source_id = file_obj["id"],
-                                        target_path = to_path, 
-                                        source_snapshot = work_obj.snap)
-                    results.append("FILE COPIED: %s -> %s" % (file_obj['path'], to_path))
-                except:
-                    # print("File copy exception: %s %s" % (sys.exc_info()[0], file_obj['path']))
-                    results.append("!!FILE COPY FAILED: %s -> %s" % (file_obj['path'], to_path))
+            try:
+                to_path = file_obj['path'].replace(work_obj.start_path, work_obj.run_class.to_dir)
+                parent_path = os.path.dirname(to_path)
+                file_name = os.path.basename(to_path)
+                work_obj.run_class.create_folder(work_obj.rc, parent_path)
+                if file_obj['type'] == "FS_FILE_TYPE_DIRECTORY":
+                    work_obj.run_class.create_folder(work_obj.rc, to_path)
+                    results.append("DIRECTORY : %s -> %s" % (file_obj['path'], to_path))
+                else:
+                    try:
+                        new_f = work_obj.rc.fs.get_file_attr(path = to_path)
+                        if new_f['size'] == file_obj['size'] and \
+                                new_f['modification_time'] == file_obj['modification_time']:
+                            results.append("FILE EXISTS: %s -> %s" % (file_obj['path'], to_path))
+                            continue
+                        else:
+                            work_obj.rc.fs.delete(id_ = new_f['id'])
+                    except:
+                        if 'fs_no_such_entry_error' not in str(sys.exc_info()[1]):
+                            print("get_file_attr exception: %s %s" % (sys.exc_info(), file_obj['path']))
+                            results.append("!!FILE COPY FAILED1: %s -> %s" % (file_obj['path'], to_path))
+                        pass
+                    try:
+                        work_obj.rc.fs.create_file(dir_path=parent_path, name=file_name)
+                        work_obj.rc.fs.copy(source_id = file_obj["id"],
+                                            target_path = to_path, 
+                                            source_snapshot = work_obj.snap)
+                        results.append("FILE COPIED: %s -> %s" % (file_obj['path'], to_path))
+                    except:
+                        # if 'fs_entry_exists_error' not in str(sys.exc_info()[1]):
+                        #     print("File create/copy exception: %s %s" % (sys.exc_info(), file_obj['path']))
+                        results.append("!!FILE COPY FAILED2: %s -> %s" % (file_obj['path'], to_path))
+            except:
+                print("Other exception: %s %s" % (sys.exc_info(), file_obj['path']))
+                results.append("!!FILE COPY FAILED3: %s -> %s" % (sys.exc_info(), file_obj['path']))
 
-        if len(results) > 0:
-            with work_obj.result_file_lock:
-                fw = io.open(work_obj.LOG_FILE_NAME, "a", encoding='utf8')
-                for d in results:
-                    fw.write("%s\n" % d)
-                fw.close()
-                work_obj.action_count.value += len(results)
+        try:
+            if len(results) > 0:
+                with work_obj.result_file_lock:
+                    fw = io.open(work_obj.LOG_FILE_NAME, "a", encoding='utf8')
+                    for d in results:
+                        fw.write("%s\n" % d)
+                    fw.close()
+                    work_obj.action_count.value += len(results)
+        except:
+            print("Unable to save results exception: %s" % str(sys.exc_info()))
 
     @staticmethod
     def work_start(work_obj):
