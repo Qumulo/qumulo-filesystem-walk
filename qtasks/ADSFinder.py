34d4082e7d5171767f228533371065837b8e221b
Mike Bott <mabott@gmail.com>
2022-09-16 15:28:22 -0700

    result filename based on starting path

--- qtasks/ADSFinder.py
+++ qtasks/ADSFinder.py
@@ -4,27 +4,30 @@ import os
 
 from typing import Optional, Sequence
 
-import qumulo.commands.fs as fs
-import qumulo.rest.fs as rest_fs
-
 from . import FileInfo, Worker
 
 
 class ADSFinder:
-    FILE_NAME = "ads-finder-log.txt"
+    DEFAULT_FILE_NAME = "ads-finder-log"
+    DEFAULT_FILE_EXTENSION = ".txt"
 
     def __init__(self, in_args: Sequence[str]):
         parser = argparse.ArgumentParser(description="")
         _args = parser.parse_args(in_args)
+        # print(_args)
 
-    def get_named_streams(self, file_obj: FileInfo, work_obj: Worker) -> Optional[str]:
+    @staticmethod
+    def get_named_streams(file_obj: FileInfo, work_obj: Worker) -> Optional[str]:
         # use work_obj.rc.fs.list_named_streams()
         streams = work_obj.rc.fs.list_named_streams(path=file_obj['path'])
         result = [(f['name'], f['size']) for f in streams]
-        # result = ' '.join(['%s %s' % (r[0], r[1]) for r in res])
-        if result:
-            print(result)
         return result
+
+    @staticmethod
+    def get_logfile_name(path):
+        return ADSFinder.DEFAULT_FILE_NAME + \
+               path.replace('/', '-') + \
+               ADSFinder.DEFAULT_FILE_EXTENSION
 
     def every_batch(  # pylint: disable=no-self-use
         self, file_list: Sequence[FileInfo], work_obj: Worker
@@ -37,16 +40,18 @@ class ADSFinder:
                 for r in res:
                     mb_res.append('%s: %s' % (file_obj['path'], r))
 
+        FILE_NAME = self.get_logfile_name(work_obj.start_path)
         with work_obj.result_file_lock:
-            with io.open(ADSFinder.FILE_NAME, "a", encoding="utf8") as f:
+            with io.open(FILE_NAME, "a", encoding="utf8") as f:
                 for line in mb_res:
                     f.write(line + "\n")
             work_obj.action_count.value += action_count
 
     @staticmethod
     def work_start(_work_obj: Worker) -> None:
-        if os.path.exists(ADSFinder.FILE_NAME):
-            os.remove(ADSFinder.FILE_NAME)
+        FILE_NAME = ADSFinder.get_logfile_name(_work_obj.start_path)
+        if os.path.exists(FILE_NAME):
+            os.remove(FILE_NAME)
 
     @staticmethod
     def work_done(_work_obj: Worker) -> None:
